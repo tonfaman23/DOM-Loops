ef7b821080f5ae21c545db6ad50da5bcb070a07d
Will Baker <1will.baker@gmail.com>
2020-01-21 19:25:24 -0600

    fixed styling and new loop added

---
+++ loops.js
@@ -0,0 +1,11 @@
+console.log("loops");
+
+const colors = ['silver', 'red', 'gold', 'aqua', 'violet'];
+
+const colorLoop = () => {
+    for(let i = 0; i < colors.length; i++) {
+        console.log(colors[i]);
+    }
+};
+
+colorLoop();
